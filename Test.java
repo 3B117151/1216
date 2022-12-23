8364419163f98f449063de2dcda57c7cad212bc3
3B117151 <114204306+3B117151@users.noreply.github.com>
2022-12-23 11:47:33 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,16 +1,45 @@
-public class Test {
-	public static void main (String[] args){
-		int [] myArr = {7,5,4,8,1,2};
-				
-			for (int i=0;i<myArr.length/2;i++){
-				int temp = myArr[i];
-				myArr[i] = myArr[myArr.length-1-i];
-				myArr[myArr.length-1-i] = temp;
-			}
-			
-			for (int i=0;i<myArr.length;i++){
-					System.out.println(myArr[i]);
+public class Test{
+	public static void main(String[] args){
+		int[] myArr ={7,5,4,9,8,1,2};
+		int[] flag =new int [myArr.length];
+		int flag_count =1;
+		
+		int max=0;
+		int min=10;
+		int sum=0;
+		float avg;
+		for(int i=0;i<myArr.length;i++){
+			if(myArr[i]>max)
+				max=myArr[i];
+			if(myArr[i]<min)
+				min=myArr[i];
+			sum += myArr[i];
+		}
+		avg = (float)sum/myArr.length;
+		
+		//==============================
+		int med=0;
+		for (int i=0;i<4;i++){
+			int flag_min=1000;
+			int flag_min_index=0;
+			for (int j=0;j<myArr.length;j++){
+				if(myArr[j]< flag_min&&flag[j]==0){
+					flag_min=myArr[j];
+					flag_min_index = j;
 				}
 			}
-	}	
+			flag[flag_min_index] =flag_count;
+			flag_count++;
+			med = myArr[flag_min_index];
+		}
 		
+		System.out.println("max:"+max);
+		System.out.println("min:"+min);
+		System.out.println("med:"+med);
+		System.out.println("avg:"+avg);
+		for(int i=0;i<flag.length;i++){
+			System.out.println("flag:"+flag[i]);
+		}
+	}
+}
+//max min med avg
